1f03ab039da6127b3e818ae4a0678169e36cf100
Javier Mazariegos <javiermazariegos@ufm.edu>
2023-04-19 17:09:04 -0600

    Update tablas.sql

--- tablas.sql
+++ tablas.sql
@@ -3,129 +3,157 @@
 BEGIN;
 
 
+DROP TABLE IF EXISTS public."BENEFICIOS";
+
 CREATE TABLE IF NOT EXISTS public."BENEFICIOS"
 (
     id bigint NOT NULL,
-    nombre_beneficio character(50)[] COLLATE pg_catalog."default",
-    is_covered bigint,
-    quant_limit_on_svc bigint,
-    explicacion character(150)[] COLLATE pg_catalog."default",
-    is_excel_from_inn_moop bigint,
-    is_excel_from_onn_moop bigint,
-    plan_id character(50)[] COLLATE pg_catalog."default",
+    nombre_beneficio character(50) COLLATE pg_catalog."default",
+    is_covered character(15) COLLATE pg_catalog."default",
+    quant_limit_on_svc character(15) COLLATE pg_catalog."default",
+    explicacion character(150) COLLATE pg_catalog."default",
+    is_excel_from_inn_moop character(15) COLLATE pg_catalog."default",
+    is_excel_from_onn_moop character(15) COLLATE pg_catalog."default",
+    plan_id character(50) COLLATE pg_catalog."default",
     CONSTRAINT "BENEFICIOS_pkey" PRIMARY KEY (id)
 );
+
+DROP TABLE IF EXISTS public."CANTIDADES";
 
 CREATE TABLE IF NOT EXISTS public."CANTIDADES"
 (
     id bigint NOT NULL,
     limite bigint,
-    unidad_limite character(50)[] COLLATE pg_catalog."default",
+    unidad_limite character(50) COLLATE pg_catalog."default",
     beneficio_id bigint,
     CONSTRAINT "CANTIDADES_pkey" PRIMARY KEY (id)
 );
+
+DROP TABLE IF EXISTS public."COSTOS";
 
 CREATE TABLE IF NOT EXISTS public."COSTOS"
 (
     id bigint NOT NULL,
     beneficio_id bigint,
-    copay_inn_tier1 character(1)[] COLLATE pg_catalog."default",
-    copay_inn_tier2 character(1)[] COLLATE pg_catalog."default",
-    copay_out_of_net character(1)[] COLLATE pg_catalog."default",
-    coins_inn_tier1 bigint,
-    coins_inn_tier2 bigint,
-    cost_out_of_net bigint,
+    copay_inn_tier1 character(15) COLLATE pg_catalog."default",
+    copay_inn_tier2 character(15) COLLATE pg_catalog."default",
+    copay_out_of_net character(15) COLLATE pg_catalog."default",
+    coins_inn_tier1 character(15) COLLATE pg_catalog."default",
+    coins_inn_tier2 character(15) COLLATE pg_catalog."default",
+    cost_out_of_net character(15) COLLATE pg_catalog."default",
     CONSTRAINT "COSTOS_pkey" PRIMARY KEY (id)
 );
 
+DROP TABLE IF EXISTS public."EDADES";
+
 CREATE TABLE IF NOT EXISTS public."EDADES"
 (
-    id bigint NOT NULL,
+    id character(15) COLLATE pg_catalog."default" NOT NULL,
     estado_id bigint,
-    edad character(50)[] COLLATE pg_catalog."default",
+    edad character(50) COLLATE pg_catalog."default",
     cantidad_edades bigint,
-    CONSTRAINT edades_pkey PRIMARY KEY (id)
+    CONSTRAINT "EDADES_pkey" PRIMARY KEY (id)
 );
+
+DROP TABLE IF EXISTS public."EDADES_SEGUROS";
 
 CREATE TABLE IF NOT EXISTS public."EDADES_SEGUROS"
 (
     id bigint NOT NULL,
-    plan_id character(30)[] COLLATE pg_catalog."default",
-    edad character(30)[] COLLATE pg_catalog."default",
+    plan_id character(50) COLLATE pg_catalog."default",
+    edad character(15) COLLATE pg_catalog."default",
     CONSTRAINT "EDADES_SEGUROS_pkey" PRIMARY KEY (id)
 );
+
+DROP TABLE IF EXISTS public."EDUCACION";
 
 CREATE TABLE IF NOT EXISTS public."EDUCACION"
 (
     id bigint NOT NULL,
-    edad_id bigint,
-    grado character(50)[] COLLATE pg_catalog."default",
+    edad_id character(15) COLLATE pg_catalog."default",
+    grado character(50) COLLATE pg_catalog."default",
     cantidad_educacion bigint,
     CONSTRAINT "EDUCACION_pkey" PRIMARY KEY (id)
 );
 
+DROP TABLE IF EXISTS public."ESTADOS";
+
 CREATE TABLE IF NOT EXISTS public."ESTADOS"
 (
     id bigint NOT NULL,
-    nombre character(50)[] COLLATE pg_catalog."default",
+    nombre character(50) COLLATE pg_catalog."default",
     CONSTRAINT "ESTADOS_pkey" PRIMARY KEY (id)
 );
+
+DROP TABLE IF EXISTS public."ESTADOS_CIVILES";
 
 CREATE TABLE IF NOT EXISTS public."ESTADOS_CIVILES"
 (
     id bigint NOT NULL,
-    edad_id bigint,
-    estado_civil character(50)[] COLLATE pg_catalog."default",
+    edad_id character(15) COLLATE pg_catalog."default",
+    estado_civil character(50) COLLATE pg_catalog."default",
     cantidad_civil bigint,
     CONSTRAINT "ESTADOS_CIVILES_pkey" PRIMARY KEY (id)
 );
 
+DROP TABLE IF EXISTS public."ETNIAS";
+
 CREATE TABLE IF NOT EXISTS public."ETNIAS"
 (
     id bigint NOT NULL,
-    edad_id bigint,
-    etnia character(50)[] COLLATE pg_catalog."default",
+    edad_id character(15) COLLATE pg_catalog."default",
+    etnia character(50) COLLATE pg_catalog."default",
     cantidad_etnias bigint,
     CONSTRAINT "ETNIAS_pkey" PRIMARY KEY (id)
 );
+
+DROP TABLE IF EXISTS public."FECHAS_TASAS";
 
 CREATE TABLE IF NOT EXISTS public."FECHAS_TASAS"
 (
     id bigint NOT NULL,
     edad_seguro_id bigint,
-    fecha_efectiva character(15)[] COLLATE pg_catalog."default",
-    fecha_expiracion character(15)[] COLLATE pg_catalog."default",
+    fecha_efectiva character(15) COLLATE pg_catalog."default",
+    fecha_expiracion character(15) COLLATE pg_catalog."default",
     CONSTRAINT "FECHAS_TASAS_pkey" PRIMARY KEY (id)
 );
+
+DROP TABLE IF EXISTS public."HOGARES";
 
 CREATE TABLE IF NOT EXISTS public."HOGARES"
 (
     id bigint NOT NULL,
-    edad_id bigint,
-    personas character(50)[] COLLATE pg_catalog."default",
-    camtidad_hogares bigint,
+    edad_id character(15) COLLATE pg_catalog."default",
+    personas character(50) COLLATE pg_catalog."default",
+    cantidad_hogares bigint,
     CONSTRAINT "HOGARES_pkey" PRIMARY KEY (id)
 );
+
+DROP TABLE IF EXISTS public."INGRESOS";
 
 CREATE TABLE IF NOT EXISTS public."INGRESOS"
 (
     id bigint NOT NULL,
-    edad_id bigint,
-    ingreso character(1)[] COLLATE pg_catalog."default",
-    cantidad_ingreso bigint,
+    edad_id character(15) COLLATE pg_catalog."default",
+    ingresos character(50) COLLATE pg_catalog."default",
+    cantidad_ingresos bigint,
     CONSTRAINT "INGRESOS_pkey" PRIMARY KEY (id)
 );
 
+DROP TABLE IF EXISTS public."PLANES";
+
 CREATE TABLE IF NOT EXISTS public."PLANES"
 (
-    id character(50)[] COLLATE pg_catalog."default" NOT NULL,
+    id character(25) COLLATE pg_catalog."default" NOT NULL,
     estado_id bigint,
-    nombre_estado character(50)[] COLLATE pg_catalog."default",
-    nombre_plan character(50)[] COLLATE pg_catalog."default",
-    tipo character(6)[] COLLATE pg_catalog."default",
-    nivel_metal character(15)[] COLLATE pg_catalog."default",
+    nombre_plan character(100) COLLATE pg_catalog."default",
+    tipo character(10) COLLATE pg_catalog."default",
+    nivel_metal character(15) COLLATE pg_catalog."default",
+    nombre_proveedor character(100) COLLATE pg_catalog."default",
     CONSTRAINT "PLANES_pkey" PRIMARY KEY (id)
 );
+
+DROP TABLE IF EXISTS public."TARIFAS_GRUPALES";
 
 CREATE TABLE IF NOT EXISTS public."TARIFAS_GRUPALES"
 (
@@ -135,22 +163,24 @@ CREATE TABLE IF NOT EXISTS public."TARIFAS_GRUPALES"
     beneficiario_un_dependiente double precision,
     beneficiario_dos_dependientes double precision,
     beneficiario_tres_mas_dependientes double precision,
-    pareja_un_dependiente double precision,
+    pareja_un_dependientes double precision,
     pareja_dos_dependientes double precision,
     pareja_tres_mas_dependientes double precision,
     CONSTRAINT "TARIFAS_GRUPALES_pkey" PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS public."TARIFAS_INDIVIDUALES"
+DROP TABLE IF EXISTS public."TASAS_INDIVIDUALES";
+
+CREATE TABLE IF NOT EXISTS public."TASAS_INDIVIDUALES"
 (
     id bigint NOT NULL,
     fechas_id bigint,
     tarifa double precision,
-    CONSTRAINT "TARIFAS_INDIVIDUALES_pkey" PRIMARY KEY (id)
+    CONSTRAINT "TASAS_INDIVIDUALES_pkey" PRIMARY KEY (id)
 );
 
 ALTER TABLE IF EXISTS public."BENEFICIOS"
-    ADD CONSTRAINT fk_ben_cod_plan FOREIGN KEY (plan_id)
+    ADD CONSTRAINT fk_beneficios_cod_planes FOREIGN KEY (plan_id)
     REFERENCES public."PLANES" (id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
@@ -158,7 +188,7 @@ ALTER TABLE IF EXISTS public."BENEFICIOS"
 
 
 ALTER TABLE IF EXISTS public."CANTIDADES"
-    ADD CONSTRAINT fk_can_cod_beneficios FOREIGN KEY (beneficio_id)
+    ADD CONSTRAINT fk_cantidades_cod_beneficios FOREIGN KEY (beneficio_id)
     REFERENCES public."BENEFICIOS" (id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
@@ -166,7 +196,7 @@ ALTER TABLE IF EXISTS public."CANTIDADES"
 
 
 ALTER TABLE IF EXISTS public."COSTOS"
-    ADD CONSTRAINT fk_cos_cod_beneficios FOREIGN KEY (beneficio_id)
+    ADD CONSTRAINT fk_costos_cod_beneficios FOREIGN KEY (beneficio_id)
     REFERENCES public."BENEFICIOS" (id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
@@ -174,7 +204,7 @@ ALTER TABLE IF EXISTS public."COSTOS"
 
 
 ALTER TABLE IF EXISTS public."EDADES"
-    ADD CONSTRAINT fk_eda_cod_estado FOREIGN KEY (estado_id)
+    ADD CONSTRAINT fk_edades_cod_estados FOREIGN KEY (estado_id)
     REFERENCES public."ESTADOS" (id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
@@ -182,7 +212,7 @@ ALTER TABLE IF EXISTS public."EDADES"
 
 
 ALTER TABLE IF EXISTS public."EDADES_SEGUROS"
-    ADD CONSTRAINT fk_eda_seg_cod_planes FOREIGN KEY (plan_id)
+    ADD CONSTRAINT fk_edads_cod_planes FOREIGN KEY (plan_id)
     REFERENCES public."PLANES" (id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
@@ -190,7 +220,7 @@ ALTER TABLE IF EXISTS public."EDADES_SEGUROS"
 
 
 ALTER TABLE IF EXISTS public."EDUCACION"
-    ADD CONSTRAINT fk_edu_cod_edades FOREIGN KEY (edad_id)
+    ADD CONSTRAINT fk_educacion_cod_edades FOREIGN KEY (edad_id)
     REFERENCES public."EDADES" (id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
@@ -198,7 +228,7 @@ ALTER TABLE IF EXISTS public."EDUCACION"
 
 
 ALTER TABLE IF EXISTS public."ESTADOS_CIVILES"
-    ADD CONSTRAINT fk_civ_cod_edades FOREIGN KEY (edad_id)
+    ADD CONSTRAINT fk_civiles_cod_edades FOREIGN KEY (edad_id)
     REFERENCES public."EDADES" (id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
@@ -206,7 +236,7 @@ ALTER TABLE IF EXISTS public."ESTADOS_CIVILES"
 
 
 ALTER TABLE IF EXISTS public."ETNIAS"
-    ADD CONSTRAINT fk_etn_cod_edades FOREIGN KEY (edad_id)
+    ADD CONSTRAINT fk_etnias_cod_edades FOREIGN KEY (edad_id)
     REFERENCES public."EDADES" (id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
@@ -214,7 +244,7 @@ ALTER TABLE IF EXISTS public."ETNIAS"
 
 
 ALTER TABLE IF EXISTS public."FECHAS_TASAS"
-    ADD CONSTRAINT fk_fec_cod_edades_seg FOREIGN KEY (edad_seguro_id)
+    ADD CONSTRAINT fk_fechas_cod_edads FOREIGN KEY (edad_seguro_id)
     REFERENCES public."EDADES_SEGUROS" (id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
@@ -222,7 +252,7 @@ ALTER TABLE IF EXISTS public."FECHAS_TASAS"
 
 
 ALTER TABLE IF EXISTS public."HOGARES"
-    ADD CONSTRAINT fk_hog_cod_edades FOREIGN KEY (edad_id)
+    ADD CONSTRAINT fk_hogares_cod_edades FOREIGN KEY (edad_id)
     REFERENCES public."EDADES" (id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
@@ -230,7 +260,7 @@ ALTER TABLE IF EXISTS public."HOGARES"
 
 
 ALTER TABLE IF EXISTS public."INGRESOS"
-    ADD CONSTRAINT fk_ing_cod_edades FOREIGN KEY (edad_id)
+    ADD CONSTRAINT fk_ingresos_cod_edades FOREIGN KEY (edad_id)
     REFERENCES public."EDADES" (id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
@@ -238,7 +268,7 @@ ALTER TABLE IF EXISTS public."INGRESOS"
 
 
 ALTER TABLE IF EXISTS public."PLANES"
-    ADD CONSTRAINT fk_pla_cod_estado FOREIGN KEY (estado_id)
+    ADD CONSTRAINT fk_planes_cod_estados FOREIGN KEY (estado_id)
     REFERENCES public."ESTADOS" (id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
@@ -246,15 +276,15 @@ ALTER TABLE IF EXISTS public."PLANES"
 
 
 ALTER TABLE IF EXISTS public."TARIFAS_GRUPALES"
-    ADD CONSTRAINT fk_tas_gru_cod_fechas FOREIGN KEY (fechas_id)
+    ADD CONSTRAINT fk_tarifasg_cod_fechas FOREIGN KEY (fechas_id)
     REFERENCES public."FECHAS_TASAS" (id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
     NOT VALID;
 
 
-ALTER TABLE IF EXISTS public."TARIFAS_INDIVIDUALES"
-    ADD CONSTRAINT fk_tas_ind_cod_fechas FOREIGN KEY (fechas_id)
+ALTER TABLE IF EXISTS public."TASAS_INDIVIDUALES"
+    ADD CONSTRAINT fk_tarifasi_cod_fechas FOREIGN KEY (fechas_id)
     REFERENCES public."FECHAS_TASAS" (id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
